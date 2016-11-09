16b9370f05c6ccd9d11b5684589b2af0be4c6e29
Jerry <jjames34@illinois.edu>
2016-11-08 20:12:01 -0600

    Initial Commit

---
+++ RideShare/app/src/main/java/sudo_rm_rf/rideshare/NewsFeed.java
@@ -0,0 +1,13 @@
+package sudo_rm_rf.rideshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NewsFeed extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_feed);
+    }
+}

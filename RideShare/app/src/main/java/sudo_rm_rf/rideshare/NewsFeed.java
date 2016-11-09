4407be977c574c6f17881d771757d979b08f980c
Jerry <jjames34@illinois.edu>
2016-11-08 22:48:42 -0600

    Added the assets

--- RideShare/app/src/main/java/sudo_rm_rf/rideshare/NewsFeed.java
+++ RideShare/app/src/main/java/sudo_rm_rf/rideshare/NewsFeed.java
@@ -2,11 +2,15 @@ package sudo_rm_rf.rideshare;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
 
 public class NewsFeed extends AppCompatActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        mListView = (ListView) findViewById(R.id.driver_list_view);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_news_feed);
     }

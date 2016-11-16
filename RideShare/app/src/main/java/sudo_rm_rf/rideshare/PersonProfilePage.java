6e46174d7404c390cf88fc63a8453c244f8e17f4
Jerry <jjames34@illinois.edu>
2016-11-15 23:18:17 -0600

    Implemented Personal Profile Pictures

---
+++ RideShare/app/src/main/java/sudo_rm_rf/rideshare/PersonProfilePage.java
@@ -0,0 +1,13 @@
+package sudo_rm_rf.rideshare;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PersonProfilePage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_profile_page);
+    }
+}
